46eff2f7d11548392ca47855c85210de6c03588a
Viktor Nemet <dajlefromcb@gmail.com>
2020-02-19 22:21:46 +0100

    valogato-interface kész

---
+++ First/01/valogato-interface/src/main/java/hu/me/krz/haladojava/Proba.java
@@ -0,0 +1,15 @@
+package hu.me.krz.haladojava;
+
+public class Proba {
+
+	public static void main(String[] args) {
+		
+		if (args.length != 1) {
+			System.exit(0);
+		}
+		
+		Szemely[] jok = new Szemely[args.length];
+		
+	}
+
+}
