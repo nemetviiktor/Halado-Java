94a257d98ea612c6ad1cffc7b166fd27ddcfe38b
Viktor Nemet <dajlefromcb@gmail.com>
2020-04-04 11:36:13 +0200

    implement

---
+++ 02/komplex/src/main/java/UserDto.java
@@ -0,0 +1,15 @@
+public class UserDto {
+    public UserDto(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    String name;
+}
