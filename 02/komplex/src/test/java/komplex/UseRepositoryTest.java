44bafbc859386c53684c3bf7ed50e3d5e089eda7
Viktor Nemet <dajlefromcb@gmail.com>
2020-04-05 22:37:20 +0200

    userrepotest completed

--- 02/komplex/src/test/java/komplex/UseRepositoryTest.java
+++ 02/komplex/src/test/java/komplex/UseRepositoryTest.java
@@ -16,35 +16,51 @@ import java.util.List;
 
 class UseRepositoryTest {
 	
-	@Mock
-	List<User> users;
-	
-	@Mock
-	UserModify um;
 
 	@Test
 	public void saveTest() {
 		
 		UseRepository ur = new UseRepository();
-		User u = mock(User.class);
+		User user = mock(User.class);
 		
-		ur.save(u);
+		when(user.getName()).thenReturn("Test");
 		
-		verify(u);
+		ur.save(user);
+		
+		assertEquals(user, ur.findAll().get(0));
+		assertEquals(1, ur.findAll().size());
 	}
 	
 	@Test
 	public void getByNektunKodTest() {
 		
-		List<User> users = new ArrayList<>();
-		
 		UseRepository ur = new UseRepository();
 		User user = mock(User.class);
 		
-		when(user.getNeptunkod()).thenReturn("neptunkod");
+		ur.save(user);
 		
-		assertEquals(user, ur.getByNektunKod("1"));
+		when(user.getNeptunkod()).thenReturn("true");
 		
+		assertEquals(user, ur.getByNektunKod("true"));
+		assertEquals(null, ur.getByNektunKod("false"));
 	}
+	
+	@Test
+	public void findByEnabledIsTrueTest() {
+		
+		UseRepository ur = new UseRepository();
+		User user1 = mock(User.class);
+		User user2 = mock(User.class);
+		
+		ur.save(user1);
+		ur.save(user2);
+		
+		when(user1.isEnabled()).thenReturn(true);
+		when(user2.isEnabled()).thenReturn(false);
+		
+		assertEquals(user1, ur.findByEnabledIsTrue().get(0));
+		assertEquals(1, ur.findByEnabledIsTrue().size());
+	}
+	
 
 }
